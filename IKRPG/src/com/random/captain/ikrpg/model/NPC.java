0934a15578c2f8ea676658ed03faeae4f48dc758
Ben Simonsen <bsimonse@vt.edu>
2013-05-12 21:43:00 -0400

    Get some constructors

--- IKRPG/src/com/random/captain/ikrpg/model/NPC.java
+++ IKRPG/src/com/random/captain/ikrpg/model/NPC.java
@@ -2,4 +2,8 @@ package com.random.captain.ikrpg.model;
 
 public class NPC extends Character
 {
+	public NPC()
+	{
+		super(null, null, null, null);
+	}
 }
--- IKRPG/src/com/random/captain/ikrpg/model/PC.java
+++ IKRPG/src/com/random/captain/ikrpg/model/PC.java
@@ -2,4 +2,8 @@ package com.random.captain.ikrpg.model;
 
 public class PC extends Character
 {
+	public PC()
+	{
+		super(null, null, null, null);
+	}
 }
