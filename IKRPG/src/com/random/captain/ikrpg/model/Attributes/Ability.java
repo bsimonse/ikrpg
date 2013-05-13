96fd902e6ca01057385ce843d5596ab8f8116471
Ben Simonsen <bsimonse@vt.edu>
2013-05-12 22:30:09 -0400

    Basic career enetered

--- IKRPG/src/com/random/captain/ikrpg/model/Attributes/Ability.java
+++ IKRPG/src/com/random/captain/ikrpg/model/Attributes/Ability.java
@@ -1,5 +1,28 @@
 package com.random.captain.ikrpg.model.Attributes;
 
-public class Ability
+public enum Ability
 {
+	JACK_MARSHALL("'Jack Marshall", "Kickin'"),
+	ACE_COMMANDER("Ace Commander", "Whee"),
+	ACROBATICS("Acrobatics","Jump"),
+	BOMBER("Bomber",""),
+	BREW_MASTER("Brew Master",""),
+	FAST_COOK("Fast Cook",""),
+	FIELD_ALCHEMIST("Field Alchemist", ""),
+	FIRE_IN_THE_HOLE("Fire in the Hole!",""),
+	FREE_STYLE("Free Style",""),
+	GRENADIER("Grenadier","Boom"),
+	POISON_RESISTANCE("Poison Resistance", "Hiss");
+	
+	private Ability(String pName, String pDesc)
+	{
+		name = pName;
+		description = pDesc;
+	}
+	
+	private String name;
+	private String description;
+	
+	public String getName(){return name;}
+	public String getDescription(){return description;}
 }
--- IKRPG/src/com/random/captain/ikrpg/model/Attributes/Career.java
+++ IKRPG/src/com/random/captain/ikrpg/model/Attributes/Career.java
@@ -1,5 +1,34 @@
 package com.random.captain.ikrpg.model.Attributes;
 
-public class Career
+import android.util.Pair;
+import java.util.ArrayList;
+import java.util.Collection;
+
+public enum Career
 {
+	ALCHEMIST("Alchemist",
+				new Pair[] {Pair.create(Skill.HAND_WEAPON, 1), Pair.create(Skill.THROWN_WEAPON, 1), Pair.create(Skill.ALCHEMY, 1), Pair.create(Skill.MEDICINE, 1)},
+				new Ability[] {Ability.GRENADIER, Ability.POISON_RESISTANCE},
+				new Pair[] {Pair.create(Skill.HAND_WEAPON, 2), Pair.create(Skill.THROWN_WEAPON, 2), Pair.create(Skill.UNARMED, 2), Pair.create(Skill.ALCHEMY, 4),
+							Pair.create(Skill.CRAFT, 4), Pair.create(Skill.FORGERY, 2), Pair.create(Skill.MEDICINE,4), Pair.create(Skill.NEGOTIATION, 4),
+							Pair.create(Skill.RESEARCH, 4)},
+				new Ability[] {Ability.BOMBER, Ability.BREW_MASTER, Ability.FAST_COOK, Ability.FIELD_ALCHEMIST, Ability.FIRE_IN_THE_HOLE, Ability.FREE_STYLE,
+								Ability.GRENADIER, Ability.POISON_RESISTANCE}),
+								
+	ARCANE_MECHANIK("Arcane Mechanik",null,null,null,null);
+	
+	private Career(String pName,Pair<Skill, Integer>[] pStartSkills, Ability[] pStartAbilities, Pair<Skill, Integer>[] pSkills, Ability[] pAbilities)
+	{
+		name = pName;
+		startingSkills = pStartSkills;
+		startingAbilities = pStartAbilities;
+		careerSkills = pSkills;
+		careerAbilities = pAbilities;
+	}
+	
+	private String name;
+	private Pair<Skill, Integer>[] startingSkills;
+	private Ability[] startingAbilities;
+	private Pair<Skill, Integer>[] careerSkills;
+	private Ability[] careerAbilities;
 }
